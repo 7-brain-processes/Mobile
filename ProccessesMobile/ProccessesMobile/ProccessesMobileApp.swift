47437eb7deb0319b8d72582da9ea1817991b3494
Dark-Type <SKAZHUTIN.N@MAIL.RU>
2026-03-06 08:30:12 +0700

    Initial commit

---
+++ ProccessesMobile/ProccessesMobile/ProccessesMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProccessesMobileApp.swift
+//  ProccessesMobile
+//
+//  Created by dark type on 06.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct ProccessesMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
